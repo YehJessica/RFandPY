1fc116f483c4d5b51b052df9e044069acf7c4890
YehJessica <cdcd64811@gmail.com>
2022-06-27 20:06:27 +0800

    6/27testPR

--- requestdata.py
+++ requestdata.py
@@ -1,18 +1,20 @@
 import requests
 
+
 def getrequestdata():
-    respones = requests.get('https://shopee.tw/api/v4/search/trending_search?bundle=popsearch&offset=0', verify=True)
-    obj=respones.json()
+    respones = requests.get(
+        'https://shopee.tw/api/v4/search/trending_search?bundle=popsearch&offset=0', verify=True)
+    obj = respones.json()
 #   print(type(obj)) #dict
 #   print(type(obj['data'])) #'dict'
-    dric=(obj['data'])
+    dric = (obj['data'])
 #   print(type(dric['querys']))#'list''
-#    print(dric['querys'])
+#   print(dric['querys'])
 #   print(type(dric['querys'][0])) #'dict'
-    count=8
+    count = 8
 
     results = []
 
     for i in range(count):
-      results.append(dric['querys'][i]['text'])
+        results.append(dric['querys'][i]['text'])
     return results
